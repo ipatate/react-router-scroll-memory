50de2c9a692220eeb76e0193a3a8a25c4f1fa9dd
Patrick Faramaz <faramazpat@gmail.com>
2018-10-07 16:34:56 +0200

    correction exemple file app

--- exemple/src/App.js
+++ exemple/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { injectGlobal } from 'styled-components';
-// import ScrollMemory from 'react-router-scroll-memory';
-import ScrollMemory from './dist/ScrollMemory.min.js';
+import ScrollMemory from 'react-router-scroll-memory';
 import Home from './Home';
 import Details from './Details';
 import About from './About';
