fa3318dc889eb35f1b5fa36a6c68150eb8dc8c40
Patrick Faramaz <faramazpat@gmail.com>
2018-11-06 15:45:54 +0100

    update all package, update code for scroll memory: now stock key and scroll position in Map. stop using componentWillReceiveProps instead shouldComponentUpdate for react 16 compatibility.

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/ScrollMemory.js',
   output: {
     format: 'cjs',
-    file: 'dist/ScrollMemory.min.js',
+    file: 'dist/ScrollMemory.min.js'
   },
   plugins: [
     resolve(),
     babel({
-      exclude: 'node_modules/**', // only transpile our source code
+      exclude: 'node_modules/**' // only transpile our source code
     }),
-    uglify(),
+    uglify()
   ],
-  external: ['react', 'react-router-dom'],
+  external: ['react', 'react-router-dom']
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,43 +1,4 @@
 // @flow
-type PageInfo = { pathname: string, scroll: number };
-/**
- * search object and return object find or undefined
- * @param {array} url
- * @param {string} path
- * @return {object}
- */
-export const getPage = (url: Array<PageInfo> = [], path: string): ?PageInfo => {
-  const lastIndex = url.map(x => x.pathname).indexOf(path);
-  return lastIndex > -1 ? url[lastIndex] : undefined;
-};
-
-/**
- * search object and return index or -1
- * @param {array} url
- * @param {string} path
- * @return {number}
- */
-export const getIndexPage = (url: Array<PageInfo> = [], path: string): number => url.map(x => x.pathname).indexOf(path);
-
-/**
- * save path and scroll in url array
- * @param {array} url
- * @param {string} pathname
- * @param {number} scroll
- * @return {array}
- */
-export const saveUrl = (url: Array<PageInfo>, pathname: string = '', scroll: number = 0): Array<PageInfo> => {
-  const u = Array.isArray(url) ? url : [];
-  // if path exist in array replace scroll value
-  const page = getIndexPage(u, pathname);
-  if (page > -1) {
-    u[page].scroll = scroll;
-    return u;
-  }
-  // save actual url
-  u.push({ pathname, scroll });
-  return u;
-};
 
 /**
  * get the scroll of page
@@ -79,7 +40,7 @@ export const scrollTo = (scrollnumber: number = 0): number =>
  * @param {number} scrollnumber
  * @return {void}
  */
-export const scrollToElement = (scrollnumber: number = 0, elementId: string): number => {
+export const scrollToElement = (scrollnumber: number = 0, elementId: string): void => {
   const element = document.getElementById(elementId);
   if (element) {
     element.scrollTop = scrollnumber;
